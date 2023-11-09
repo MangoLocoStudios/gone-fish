25024acc7464bc3334b8cb771980b448dcba133e
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-11-09 20:33:12 +1000

    format code

--- src/fish.rs
+++ src/fish.rs
@@ -2,20 +2,24 @@ use bevy::prelude::*;
 use rand;
 
 pub fn setup_fish(mut commands: Commands, asset_server: Res<AssetServer>) {
-     for _ in 0..10 {
-         let vertical_position = rand::random::<f32>() * -400.0 + 20.0;
-         let horizontal_position = rand::random::<f32>() * -400.0 + 20.0;
-         let going_left = rand::random::<bool>();
+    for _ in 0..10 {
+        let vertical_position = rand::random::<f32>() * -400.0 + 20.0;
+        let horizontal_position = rand::random::<f32>() * -400.0 + 20.0;
+        let going_left = rand::random::<bool>();
 
-         let mut fishy = SpriteBundle {
-             texture: asset_server.load("fish4.png"),
-             transform: Transform::from_translation(Vec3::new(horizontal_position, vertical_position, 0.0))
-                 .with_scale(Vec3::new(1.0, 1.0, 1.0)),
-             ..default()
-         };
-         fishy.sprite.flip_x = going_left;
-         commands.spawn(fishy);
-     }
+        let mut fishy = SpriteBundle {
+            texture: asset_server.load("fish4.png"),
+            transform: Transform::from_translation(Vec3::new(
+                horizontal_position,
+                vertical_position,
+                0.0,
+            ))
+            .with_scale(Vec3::new(1.0, 1.0, 1.0)),
+            ..default()
+        };
+        fishy.sprite.flip_x = going_left;
+        commands.spawn(fishy);
+    }
 }
 
 pub fn update_fish(mut all_fish: Query<(&mut Sprite, &mut Transform)>, time: Res<Time>) {
@@ -31,11 +35,9 @@ pub fn update_fish(mut all_fish: Query<(&mut Sprite, &mut Transform)>, time: Res
         let stutter = rand::random::<f32>() * 200.0 * time.delta_seconds();
         transform.translation.x += stutter;
 
-        println!("x: {}", transform.translation.x);
-
         // Flip the thing when at edge
         let delta_abs = 450.0;
-        if transform.translation.x < -delta_abs  {
+        if transform.translation.x < -delta_abs {
             fish.flip_x = true;
         } else if transform.translation.x > delta_abs {
             fish.flip_x = false;

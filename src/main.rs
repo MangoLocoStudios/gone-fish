8b4207f64fff8a939bd99b034e9d20f18bbc65ce
Tylah Kapa <tylahkapa@gmail.com>
2023-11-09 20:12:22 +1000

    Revert "Abstract Game System"
    This reverts commit 396c2b711e1e0727c3dd60fff72334438f901df0.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use bevy::prelude::*;
-use crate::systems::game::GamePlugin;
+use player::{Player, PlayerMovementPlugin};
+use rod::RodPlugin;
+use crate::fish_storage::FishStorage;
 
 pub mod player;
 pub mod rod;
 pub mod fish_storage;
-pub mod port;
-pub mod systems;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, GamePlugin))
+        .add_plugins((DefaultPlugins, PlayerMovementPlugin, RodPlugin))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        Player,
+        FishStorage {
+            current: 0,
+            max: 10
+        },
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(100.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 50.0, 0.0)),
+            ..default()
+        },
+    ));
 }

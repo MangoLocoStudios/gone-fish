59539dd031870d8c8714b8bdb77d64bf9ca73cb4
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-12-01 22:32:21 +1000

    wasmgasm

--- gone-fish.d.ts
+++ gone-fish.d.ts
@@ -56,11 +56,11 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly wasm_bindgen__convert__closures__invoke0_mut__h4af240774661330e: (a: number, b: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke1_mut__h3bb64d8dbd7e02d7: (a: number, b: number, c: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke0_mut__h057e52df55f61a99: (a: number, b: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke1_mut__hdca40b718b7f71e7: (a: number, b: number, c: number) => void;
-  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he335eb0b49e26eef: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h1451077085a60ea0: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0daa1f5f4822e9dd: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__hf95e8ca1e9424950: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h96aab1492455d1f0: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8bd0c3cbee83ad7a: (a: number, b: number, c: number) => void;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_start: () => void;
